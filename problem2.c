bdfb522c94224073054165b5ce6845553a5a47f3
Md Shafiul Islam <150769484+Shafiul-p@users.noreply.github.com>
2024-07-06 08:34:01 +0600

    Update problem2.c

--- problem2.c
+++ problem2.c
@@ -1,8 +1,11 @@
+/*The formula to calculate the area of a circumference is defined as A = π . R2.
+Considering to this problem that π = 3.14159:
+Calculate the area using the formula given in the problem description.*/
+
 #include<stdio.h>
 int main(){
-   
-    double R,x;
-scanf("%lf",&R);
+    double R,x;// double is a data type
+scanf("%lf",&R);// for double datatype we need to take %lf
 x=3.14159*R*R;
-printf("A=%.4lf\n",x);
+printf("A=%.4lf\n",x); // getting 4 numbers after dacimal point we need to take .4 and %.4lf
 }
