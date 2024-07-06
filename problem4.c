85255f411f5af6e8c7a25b2ea2a4081756b2ce77
Md Shafiul Islam <150769484+Shafiul-p@users.noreply.github.com>
2024-07-06 08:38:52 +0600

    Update problem4.c

--- problem4.c
+++ problem4.c
@@ -1,3 +1,7 @@
+*/Read two integer values. After this, calculate the product between them and store the result in a variable named PROD. 
+Print the result like the example below.Do not forget to print the end of line after the result, 
+    otherwise you will receive “Presentation Error”.*/
+
 #include<stdio.h>
 int main()
 {
