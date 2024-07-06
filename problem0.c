e9bd33905eb1e857f9c7dc445fc13ae834315b6b
Md Shafiul Islam <150769484+Shafiul-p@users.noreply.github.com>
2024-07-06 08:26:40 +0600

    Update problem0.c

--- problem0.c
+++ problem0.c
@@ -1,3 +1,6 @@
+/*Your first program in any programming language is usually "Hello World!". 
+ In this first problem all you have to do is print this message on the screen.*/
+
 #include <stdio.h>
  
 int main() {
