1cb7f2652db79b7df3aebbd8f741f402aacfac53
Md Shafiul Islam <150769484+Shafiul-p@users.noreply.github.com>
2024-07-06 08:25:09 +0600

    Update problem1.c

--- problem1.c
+++ problem1.c
@@ -1,9 +1,13 @@
-  #include <stdio.h>
+/*Read 2 variables, named A and B and make the sum of these two variables, assigning its result to the variable X.
+Print X as shown below. Print endline after the result otherwise you will get “Presentation Error”.*/
+
+
+#include <stdio.h>
  
 int main() {
- int a,b,sum;
- scanf("%d %d",&a,&b);
- sum=a+b;
- printf("X = %d\n",sum);
+ int A,B,sum;//declaration variable 
+ scanf("%d %d",&A,&B);//get user values 
+ sum=A+B;//drone operation according to the question 
+ printf("X = %d\n",sum);//     \n is user for creating new line and it is essential to beecrowd problem solved 
     return 0;
 }
