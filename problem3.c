c2c661dfd153df46f6ecb52eb328c7e9449b4775
Md Shafiul Islam <150769484+Shafiul-p@users.noreply.github.com>
2024-07-06 08:36:48 +0600

    Update problem3.c

--- problem3.c
+++ problem3.c
@@ -1,9 +1,12 @@
+/*Read two integer values, in this case, the variables A and B.
+After this, calculate the sum between them and assign it to the variable SOMA. Write the value of this variable.*/
+
 #include<stdio.h>
 int main()
 {
 int A,B,sum;
 scanf("%d %d",&A,&B);
 sum=A+B;
-printf("SOMA = %d\n",sum);
+printf("SOMA = %d\n",sum);// for matching problems output we need to take SOMA = 
 return 0;
 }
