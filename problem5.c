84499a57e202a654cedd585ea52462cfddb35f14
Md Shafiul Islam <150769484+Shafiul-p@users.noreply.github.com>
2024-07-06 08:41:14 +0600

    Update problem5.c

--- problem5.c
+++ problem5.c
@@ -1,9 +1,16 @@
+*/Read two floating points' values of double precision A and B, corresponding to two student's grades.
+  After this, calculate the student's average, considering that grade A has weight 3.5 and B has weight 7.5. 
+  Each grade can be from zero to ten, always with one digit after the decimal point.
+  Don’t forget to print the end of line after the result, otherwise you will receive “Presentation Error”. 
+  Don’t forget the space before and after the equal sign.*/
+
+
 #include<stdio.h>
 int main()
 {
 double A,B,average;
 scanf("%lf %lf",&A,&B);
-average=(A*3.5+B*7.5)/(3.5+7.5);
+average=(A*3.5+B*7.5)/(3.5+7.5);//It's formula
 printf("MEDIA = %.5f\n",average);
 return 0;
 
